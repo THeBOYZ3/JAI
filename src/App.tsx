9a917b29a405a4c7f4b56154b292b703aae08919
JAIRUS <145732918+THeBOYZ3@users.noreply.github.com>
2026-05-09 07:59:12 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,6 @@ export default function App() {
   return (
     <>
       <SocialDock isOpen={leftOpen} setIsOpen={toggleLeft} />
-      <VibeRain active={isMusicPlaying} />
       <AnimatePresence mode="wait">
         {loading && (
           <LoadingScreen 
@@ -144,7 +143,7 @@ export default function App() {
 
                   <div className="pt-0 md:pt-12">
                     <p className="text-xl md:text-2xl font-light leading-relaxed mb-12 opacity-90">
-                      My name is <span className="text-white font-bold">Jairus C. Alolod</span> from <span className="text-white font-medium">PCCASHS</span>. I am a <span className="text-white font-medium">Senior High School student</span> and an <span className="text-white font-medium">aspiring Full-Stack Developer and UX/UI Designer</span>. I am working to master both <span className="text-white/60">AutoCAD and web development</span> to build a strong foundation for my future and the <span className="text-white font-bold">JAI Project</span>.
+                      My name is <span className="text-white font-bold">Jairus C. Alolod</span> from <span className="text-white font-medium">PCCASHS</span>. I am a <span className="text-white font-medium">Senior High School student</span> and an <span className="text-white font-medium">aspiring Developer</span>. I am working to master both <span className="text-white/60">AutoCAD and web development</span> to build a strong foundation for my future and the <span className="text-white font-bold">JAI Project</span>.
                     </p>
 
                     <div className="space-y-12">
